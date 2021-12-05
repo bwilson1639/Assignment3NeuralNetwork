bcc3188be0f9e77f96d6cdb2a3a65e9980fa81fc
Benjamin <bwilson1@ualberta.ca>
2021-12-05 14:23:37 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#this assignment will have two parts with two subparts
+
+#part one: training
+#   parse .txt file
+#       open file
+#       read line (string of numbers and end in # # #)
+#       organize that line and put it into an input node
