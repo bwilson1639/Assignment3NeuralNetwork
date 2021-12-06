320a36a4d8d62f8e4acf01049786a6dc58edc9b0
Benjamin <bwilson1@ualberta.ca>
2021-12-05 21:42:49 -0700

    finished node class that initializes the weights for a new node

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-#this assignment will have two parts with two subparts
+import random
 
-#part one: training
-#   parse .txt file
-#       open file
-#       read line (string of numbers and end in # # #)
-#       organize that line and put it into an input node
+
+class Node:
+
+    def __init__(self, type):
+
+        if type == 'input':
+            self.w1 = random.uniform(-1.0,1.0)
+            self.w2 = random.uniform(-1.0, 1.0)
+            self.w3 = random.uniform(-1.0, 1.0)
+            self.w4 = random.uniform(-1.0, 1.0)
+            self.w5 = random.uniform(-1.0, 1.0)
+            self.w6 = random.uniform(-1.0, 1.0)
+            self.w7 = random.uniform(-1.0, 1.0)
+            self.w8 = random.uniform(-1.0, 1.0)
+            self.w9 = random.uniform(-1.0, 1.0)
+            self.w10 = random.uniform(-1.0, 1.0)
+
+        elif type == 'hidden':
+            self.w1 =random.uniform(-1.0 , 1.0)
+            self.w2 = random.uniform(-1.0, 1.0)
+            self.w3 = random.uniform(-1.0, 1.0)
+
+        elif type == 'biasI':
+            self.w1 = 1.0
+            self.w2 = 1.0
+            self.w3 = 1.0
+            self.w4 = 1.0
+            self.w5 = 1.0
+            self.w6 = 1.0
+            self.w7 = 1.0
+            self.w8 = 1.0
+            self.w9 = 1.0
+            self.w10 = 1.0
+
+        else:
+            self.w1 = 1.0
+            self.w2 = 1.0
+            self.w3 = 1.0
