2d9b8821fe1be39445b39a0c97fd13e2d5b1ef43
Benjamin <bwilson1@ualberta.ca>
2021-12-05 23:52:05 -0700

    added basis for training, have yet to add calculation for backwards propagation

--- main.py
+++ main.py
@@ -75,3 +75,63 @@ def initializeNodeNetwork ():
 
 
     return [inputLayer, hiddenLayer]
+
+def trainingAlgorithm () :
+
+    trainingFile = open('test.txt', 'r')
+
+    lineList = trainingFile.readlines()
+
+    accuracy = 0
+    epoch = 0
+
+    nodeNetwork = initializeNodeNetwork()
+
+    while accuracy < 90:
+
+        accuracyTotal = 0
+
+        for case in lineList:
+
+            values = lineList.split()
+            temp = 0
+
+            for Node in nodeNetwork[0]:
+                if Node.value != 0:
+
+                    Node.value = values[temp]
+
+            temp = 0
+            for Node in nodeNetwork[1]:
+
+                hiddenValue = 0
+                if Node.value != 0:
+
+                    for INode in nodeNetwork[0]:
+
+                        hiddenValue = hiddenValue + INode.value * INode.weights[temp]
+
+                temp += 1
+                Node.value = hiddenValue
+
+            outputNode1 = 0
+            outputNode8 = 0
+            outputNode9 = 0
+
+            for Node in nodeNetwork[1]:
+
+                outputNode1 = outputNode1 + Node.value * Node.weights[0]
+                outputNode8 = outputNode8 + Node.value * Node.weights[1]
+                outputNode9 = outputNode9 + Node.value * Node.weights[2]
+
+            if values[-3] == 1 and outputNode1 >= outputNode8 and outputNode1 >= outputNode9:
+                accuracyTotal += 1
+            elif values[-2] == 1 and outputNode8 > outputNode1 and outputNode8 >= outputNode9:
+                accuracyTotal += 1
+            elif values[-1] == 1 and outputNode9 > outputNode1 and outputNode9 > outputNode8:
+                accuracyTotal += 1
+            else:
+                continue
+                #backwards propagate
+
+        epoch += 1
